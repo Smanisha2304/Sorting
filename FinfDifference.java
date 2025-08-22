5dd8834d8ed42ccdfb1b3de2e8924365bd97f3ac
Manisha Sharma <alwsmilemansu23@gmail.com>
2025-08-22 23:18:49 +0530

    Add files via upload

---
+++ FinfDifference.java
@@ -0,0 +1,21 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        
+        char [] a = s.toCharArray();
+         char [] b = t.toCharArray();
+         Arrays.sort(a);
+         Arrays.sort(b);
+         char r = 0;
+        // String aa = new String(a);
+        //  String bb = new String(b);
+        for(char num:a)
+        {
+            r^=num;
+        }
+        for(char num:b)
+        {
+            r^=num;
+        }
+        return r;
+    }
+}
